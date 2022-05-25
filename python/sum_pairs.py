baca1cb294b05aa4390a9ff8ab13a163ab47afd7
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-05-25 13:28:55 -0500

    added test cases

--- python/sum_pairs.py
+++ python/sum_pairs.py
@@ -1,1 +1,12 @@
-def sum_pairs():
+def sum_pairs(array, target):
+    result = []
+    for i in range(0, len(array)):
+        for j in range(i + 1, len(array)):
+            if (array[i] + array[j] == target):
+                result.append([array[i], array[j]])
+    if len(result) > 0:
+        return result
+    else:
+        return ('unable to find pairs')
+
+#print(sum_pairs([1,2,3,4,5], 9))
