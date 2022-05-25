66f433f627837d48b6161de79bd477ed950c8e94
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-05-25 13:01:15 -0500

    initial js

--- js/sumPairs.js
+++ js/sumPairs.js
@@ -1,3 +1,15 @@
-exports.sumPairs = function() {
-
+exports.sumPairs = function(array, target) {
+    let result = [];	
+    for (i = 0; i < array.length; i++){	
+      for (let j = i + 1; j < array.length; j++){
+        if (array[i] + array[j] === target){	
+          result.push([array[i], array[j]]);	
+        }	
+      }	
+    }	
+    if (result.length > 0){	
+      return result;	
+    }	
+    return('unable to find pairs');
 };
+//console.log(exports.sumPairs([1,2,3,4,5], 9))
